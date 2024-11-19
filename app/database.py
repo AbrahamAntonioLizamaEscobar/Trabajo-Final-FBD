033d96620aac42ea1af251eb9ed1842821f4715e
AbrahamAntonioLizamaEscobar <82134380+AbrahamAntonioLizamaEscobar@users.noreply.github.com>
2024-11-19 03:12:16 -0600

    1

--- app/database.py
+++ app/database.py
@@ -1,13 +1,28 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "mariadb+pymysql://lizama:abrahamlizama@localhost:3306/trabajo_final"
+# Cargar las variables de entorno desde el archivo .env
+load_dotenv()
 
+# Leer la URL de la base de datos desde el archivo .env
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL no está configurada en las variables de entorno.")
+
+# Crear el motor de conexión
 engine = create_engine(DATABASE_URL)
+
+# Configurar la sesión
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Declarar la base para los modelos
 Base = declarative_base()
 
+# Función para obtener una sesión de base de datos
 def get_db():
     db = SessionLocal()
     try:
